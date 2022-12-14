8da47853e49207886d3d6994183b51826edb451e
Ashutosh Agrawal <83363716+ashuagr@users.noreply.github.com>
2022-12-14 15:32:27 +0530

    Create Hello World.c

---
+++ Basic Program/Hello World.c
@@ -0,0 +1,10 @@
+#include<stdio.h>   //Standard Input Output
+#include<conio.h>   //Console Input Output
+
+int main()
+{
+    clrscr();     //Clear Screen
+    printf("Hello World");
+    getch();
+    return 0;
+}  
