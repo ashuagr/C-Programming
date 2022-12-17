da6ef3bffa7326adbf3bd7c0a6b62417a6dd57fb
Ashutosh Agrawal <83363716+ashuagr@users.noreply.github.com>
2022-12-17 14:05:10 +0530

    Create Increment Decrement.c

---
+++ 01.Basic Program/Increment Decrement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    clrscr();
+    int a=10;
+    printf("Before Increment %d\n",a);
+    a++'
+    printf("After Increment %d",a);
+    a--;
+    printf("After Decrement %d",a);
+    getch();
+    return 0;
+} 
+    
