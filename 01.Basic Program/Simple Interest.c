6e4752721b30dc6931d46d65b52e32fdd7a9348f
Ashutosh Agrawal <83363716+ashuagr@users.noreply.github.com>
2022-12-17 16:17:38 +0530

    Create Simple Interest.c

---
+++ 01.Basic Program/Simple Interest.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    clrscr();
+    
+    int principle;
+    printf("Enter The  Principle Value:");
+    scanf("%d",&principle);
+    
+    int rate;
+    printf("Enter The Rate:");
+    scanf("%d",&rate);
+    
+    int time;
+    printf("Enter The Time:");
+    scanf("%d",&time);
+    
+    int SI;
+    si=(principle*rate*time)/100;
+    printf("Simple Interest=%d",SI);
+    getch();
+    return 0;
+ }   
